5f6c584524fd74cc5efa70da66fddc4d19dd953a
Emmanuel Ogidan <emmanuelogidan@bepeerless.co>
2025-01-11 01:47:52 +0100

    initial commit for the movie CURD

---
+++ Go-movies-crud/go.mod
@@ -0,0 +1,5 @@
+module Go-movies-crud
+
+go 1.23.2
+
+require github.com/gorilla/mux v1.8.1 // indirect
---
+++ Go-movies-crud/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"github.com/gorilla/mux"
+)
+
+type Movie struct {
+	Id       string    `json:"id"`
+	Isbn     string    `json:"isbn"`
+	Title    string    `json:"title"`
+	Director *Director `json:"director"`
+}
+
+type Director struct {
+	firstname string `json:"firstname"`
+	lastname  string `json:"lastname"`
+}
+
+var movies []Movie
+
+func main() {
+	r := mux.NewRouter()
+
+	r.HandleFunc("/movies", getMovies).Methods("GET")
+	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
+	r.HandleFunc("/movies", createMovie).Methods("POST")
+	r.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
+	r.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
+
+}
